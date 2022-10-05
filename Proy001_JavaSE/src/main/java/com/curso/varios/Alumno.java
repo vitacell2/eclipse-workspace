26b81a95d342c777408c3f7d46cafa8776ee0ab8
vitacell2 <vita6cell@gmail.com>
2022-10-05 13:51:59 +0200

    1

---
+++ Proy001_JavaSE/src/main/java/com/curso/varios/Alumno.java
@@ -0,0 +1,22 @@
+package com.curso.varios;
+
+public class Alumno {
+
+	private int id;
+	
+	public Alumno (int id)
+	{
+		this.id = id;
+	}
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+	
+}
---
+++ Proy003_banco/src/main/java/com/banco/domain/Banking.java
@@ -0,0 +1,19 @@
+package com.banco.domain;
+
+public class Banking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		informar(informacion);
+		
+	}
+	
+	public void informar(String informacion)
+	{
+		System.out.println(".....");
+        System.out.println(informacion);
+        System.out.println("....");
+	}
+
+}
